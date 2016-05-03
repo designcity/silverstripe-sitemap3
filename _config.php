1e58665d32abc5add12a82f40de0fc3ca6108747
Michael Bollig <michael@designcity.com.au>
2016-05-03 10:58:16 +0800

    Minor PSR-2 fix

--- _config.php
+++ _config.php
@@ -1,4 +1,4 @@
 <?php
 
-ShortcodeParser::get()->register('SiteMap',array('SiteMapPage','SiteMapShortCodeHandler'));
+ShortcodeParser::get()->register('SiteMap', array('SiteMapPage', 'SiteMapShortCodeHandler'));
 define('SITEMAP3_DIR', basename(dirname(__FILE__)));
